1b82d1fc97142dd680cef1d03142b0e7f323d3fe
Victor <digitloft@gmail.com>
2015-01-06 17:08:04 +0100

    Removed SDL_gfx dependency

--- scene.h
+++ scene.h
@@ -1,6 +1,5 @@
 #include <Python.h>
 #include <SDL/SDL.h>
-#include "SDL/SDL_gfxPrimitives.h"
 
 int SX = 640;
 int SY = 480;
@@ -13,10 +12,11 @@ typedef struct {
 } RGBA_Color;
 
 SDL_Surface* screen;
+
 SDL_Rect     bounds;
 SDL_Event    event;
 
-RGBA_Color    fill_color   = {255, 255, 255, 255};
-RGBA_Color    stroke_color = {255, 255, 255, 255};
-char          fill         = 1;
-char          stroke       = 0;
+RGBA_Color   fill_color   = {255, 255, 255, 255};
+RGBA_Color   stroke_color = {255, 255, 255, 255};
+char         fill         = 1;
+char         stroke       = 0;
