c2b8660edd73d95e6756a708eef4d2dcf93beb46
Victor <digitloft@gmail.com>
2015-01-06 12:31:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+module1 = Extension("scene",
+                    libraries = ["SDL", "SDL_gfx"],
+                    sources = ["scene.c"])
+
+setup (name = "scene",
+       version = "0.1",
+       description = "Python bindings for SDL",
+       author = "Victor Kindhart",
+       author_email = "vikkindhart@gmail.com",
+       long_description = """\
+Python bindings for SDL allowing to
+run games made with `scene' library.
+"""
+       ext_modules = [module1])
